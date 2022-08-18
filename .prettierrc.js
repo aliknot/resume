d156d98ef02319b00a09c19d01c533fe2d5f3b9f
alirezafz79 <alirezafazel379@gmail.com>
2022-08-18 12:59:14 +0430

    added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	jsxBracketSameLine: true,
+	trailingComma: 'all',
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	endOfLine: 'lf',
+};
