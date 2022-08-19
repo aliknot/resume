c871670126bc58efcf30deb70579f6d345f51087
alirezafz79 <alirezafazel379@gmail.com>
2022-08-19 18:16:54 +0430

    added theme scss file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	sassOptions: {
+		additionalData: `@import "src/scss/theme.scss";`,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
