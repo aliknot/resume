846c168ee953b5e9c0438229bd025090cd63ae08
alirezafz79 <alirezafazel379@gmail.com>
2022-08-20 19:53:01 +0430

    added font and font icon styles in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../scss/globals.scss';
+import '../scss/inter.scss';
+import '../../public/assets/font-icon/style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
