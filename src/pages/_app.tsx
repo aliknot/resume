51ab8d880910f2371a413dcbf1cb64c2008783f3
alirezafz79 <alirezafazel379@gmail.com>
2022-08-26 13:09:48 +0430

    changed my app function syntax to arrow function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import '../scss/globals.scss';
 import '../scss/inter.scss';
 import '../../public/assets/font-icon/style.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
